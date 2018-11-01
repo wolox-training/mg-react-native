61a08642505e9a5207d9a6c15b3984dda2771e66
Matias Grote <mgrote@Mac-mini-de-wolox.local>
2018-11-01 17:31:37 -0300

    developed functions for min copy reverseMerge and filterAttribs

--- finger-exercises-react/react/ecmascript/01 - spread/index.js
+++ finger-exercises-react/react/ecmascript/01 - spread/index.js
@@ -1,10 +1,26 @@
 import { isArray } from './utils';
 
-export function min() {
-  isArray();
-  return Math.min();
+export function min(...theArgs) {
+  if (theArgs.length === 0) {
+    return undefined;
+  } else if (isArray(theArgs[0])) {
+    return Math.min(...theArgs[0]);
+  }
+  return Math.min(...theArgs);
 }
 
-export function copy() {
+export function copy(obj) {
+  return isArray(obj) ? [...obj] : { ...obj };
+}
 
+export function reverseMerge(obj1, obj2) {
+  const newArray = [...obj2, ...obj1];
+  return newArray;
+}
+
+export function filterAttribs(obj) {
+  const {
+    a, b, c, d, ...z
+  } = { ...obj };
+  return { c, d, ...z };
 }
