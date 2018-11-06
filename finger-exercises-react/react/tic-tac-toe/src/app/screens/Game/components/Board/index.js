ef6d4437e0b162aec32dc5280722db54d65f8c9a
Matias Grote <mgrote@Mac-mini-de-wolox.local>
2018-11-06 16:27:20 -0300

    tic tac toe adding styles

---
+++ finger-exercises-react/react/tic-tac-toe/src/app/screens/Game/components/Board/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import Square from '../Square';
+import styles from '../../styles.scss';
+
+class Board extends React.Component {
+  // eslint-disable-next-line class-methods-use-this
+  renderSquare() {
+    return <Square />;
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className={styles.status}>{status}</div>
+        <div className={styles.boardRow}>
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className={styles.boardRow}>
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className={styles.boardRow}>
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Board;
---
+++ finger-exercises-react/react/tic-tac-toe/src/app/screens/Game/components/Square/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from '../../styles.scss';
+
+class Square extends React.Component {
+  render() {
+    return <button className={styles.square}>{/* TODO */}</button>;
+  }
+}
+
+export default Square;
