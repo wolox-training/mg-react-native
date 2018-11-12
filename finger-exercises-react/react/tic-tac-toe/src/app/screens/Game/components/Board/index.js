8d584fe741878eccad6c90e2fc12e7e1313bd7d4
Matias Grote <mgrote@Mac-mini-de-wolox.local>
2018-11-12 18:24:17 -0300

    add redux to already working tic-tac-toe project

--- finger-exercises-react/react/tic-tac-toe/src/app/screens/Game/components/Board/index.js
+++ finger-exercises-react/react/tic-tac-toe/src/app/screens/Game/components/Board/index.js
@@ -7,17 +7,6 @@ import styles from './styles.scss';
 import { calculateWinner } from './utils';
 
 class Board extends React.Component {
-  handleClick(i) {
-    const history = this.state.history;
-    const current = history[history.length - 1];
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({ history: history.concat([{ squares }]), xIsNext: !this.state.xIsNext });
-  }
-
   renderSquare(i) {
     return <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />;
   }
--- finger-exercises-react/react/tic-tac-toe/src/index.js
+++ finger-exercises-react/react/tic-tac-toe/src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
+import rootReducer from './redux/reducer';
 import './scss/index.scss';
 import registerServiceWorker from './registerServiceWorker';
 
 import App from '~components/App'; // eslint-disable-line import/first
+/* eslint-disable no-underscore-dangle */
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+/* eslint-enable no-underscore-dangle */
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
